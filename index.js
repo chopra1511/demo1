6c3ea1c7ba8c8d10b5d7e2c1ae046b0ddf208384
Rahul Chopra <63909178+chopra1511@users.noreply.github.com>
2021-11-07 23:25:09 +0530

    performed getElementByID

---
+++ index.js
@@ -0,0 +1,12 @@
+// var headerTitle = document.getElementById('header-title');
+// headerTitle.textContent = "Hello";
+// headerTitle.innerText = "Bye";
+// headerTitle.innnerHTML = '<h1>Hey</h1>';
+
+
+var header = document.getElementById('main-header');
+header.style.border = 'solid 10px #000';
+
+var title = document.getElementById('main');
+title.style.fontWeigth = 'bold';
+title.style.color = 'green';
