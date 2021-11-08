ee07ba6e05c60ecd47fd1af9c9aa8403551cbee9
Rahul Chopra <63909178+chopra1511@users.noreply.github.com>
2021-11-08 20:03:44 +0530

    getElementByClassName and TagName

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ header.style.border = 'solid 10px #000';
 var title = document.getElementById('main');
 title.style.fontWeigth = 'bold';
 title.style.color = 'green';
+
+
+var items = document.getElementByClassName('list-group-items');
+console.log(items);
+items[2].style.backgroundColor = "green";
+
+for(let i = 0;i<items.length;i++){
+	items[i].style.fontWeigth = 'bold';
+}
+
+
+var li = document.getElementByTagName('li');
+console.log(li);
+li[2].style.backgroundColor = "green";
+
+for(let i = 0;i<li.length;i++){
+	li[i].style.fontWeigth = 'bold';
+}
