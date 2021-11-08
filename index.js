e25968f7eac59f5813ae5fd2e7673698b4e8cf70
Rahul Chopra <63909178+chopra1511@users.noreply.github.com>
2021-11-08 20:04:50 +0530

    getElementById

--- index.js
+++ index.js
@@ -12,19 +12,4 @@ title.style.fontWeigth = 'bold';
 title.style.color = 'green';
 
 
-var items = document.getElementByClassName('list-group-items');
-console.log(items);
-items[2].style.backgroundColor = "green";
 
-for(let i = 0;i<items.length;i++){
-	items[i].style.fontWeigth = 'bold';
-}
-
-
-var li = document.getElementByTagName('li');
-console.log(li);
-li[2].style.backgroundColor = "green";
-
-for(let i = 0;i<li.length;i++){
-	li[i].style.fontWeigth = 'bold';
-}
